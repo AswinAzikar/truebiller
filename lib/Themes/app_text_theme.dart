d7b27d70c45ebaae454a3bc2250c3a67141e90b5
Aswinazikar <azwinaswin8@gmail.com>
2024-10-23 09:46:58 +0530

    "Removed font files, updated pubspec.yaml to include font assets, and refactored landing page UI with animations and styling changes."

--- lib/Themes/app_text_theme.dart
+++ lib/Themes/app_text_theme.dart
@@ -3,26 +3,59 @@ import 'package:truebiller/utils/size_utils.dart';
 
 class AppTextStyles {
   static String openSansFontFamily = "OpenSans";
+  static String interFontFamily = "Inter";
 
+  // Open Sans Text Styles
   static TextStyle openSansRegular14 = TextStyle(
       fontFamily: openSansFontFamily,
       fontWeight: FontWeight.w400,
       fontSize: 14.fSize);
+
   static TextStyle openSansBold16 = TextStyle(
       fontFamily: openSansFontFamily,
       fontWeight: FontWeight.w700,
       fontSize: 16.fSize);
+
   static TextStyle openSansBoldItalic16 = TextStyle(
       fontFamily: openSansFontFamily,
       fontWeight: FontWeight.w700,
       fontStyle: FontStyle.italic,
       fontSize: 16.fSize);
+
   static TextStyle openSansMedium18 = TextStyle(
       fontFamily: openSansFontFamily,
       fontWeight: FontWeight.w500,
       fontSize: 18.fSize);
+
   static TextStyle openSansSemiBold24 = TextStyle(
       fontFamily: openSansFontFamily,
+      fontWeight: FontWeight.w600,
+      fontSize: 24.fSize);
+
+  // Inter Text Styles
+  static TextStyle interRegular14 = TextStyle(
+      fontFamily: interFontFamily,
+      fontWeight: FontWeight.w400,
+      fontSize: 14.fSize);
+
+  static TextStyle interBold16 = TextStyle(
+      fontFamily: interFontFamily,
+      fontWeight: FontWeight.w700,
+      fontSize: 16.fSize);
+
+  static TextStyle interBoldItalic16 = TextStyle(
+      fontFamily: interFontFamily,
+      fontWeight: FontWeight.w700,
+      fontStyle: FontStyle.italic,
+      fontSize: 16.fSize);
+
+  static TextStyle interMedium18 = TextStyle(
+      fontFamily: interFontFamily,
+      fontWeight: FontWeight.w500,
+      fontSize: 18.fSize);
+
+  static TextStyle interSemiBold24 = TextStyle(
+      fontFamily: interFontFamily,
       fontWeight: FontWeight.w600,
       fontSize: 24.fSize);
 
@@ -30,11 +63,19 @@ class AppTextStyles {
 }
 
 extension TextStyleExtensions on BuildContext {
+  // Open Sans Extensions
   TextStyle get openSansRegular14 => AppTextStyles.openSansRegular14;
   TextStyle get openSansBold16 => AppTextStyles.openSansBold16;
   TextStyle get openSansBoldItalic16 => AppTextStyles.openSansBoldItalic16;
   TextStyle get openSansMedium18 => AppTextStyles.openSansMedium18;
   TextStyle get openSansSemiBold24 => AppTextStyles.openSansSemiBold24;
+
+  // Inter Extensions
+  TextStyle get interRegular14 => AppTextStyles.interRegular14;
+  TextStyle get interBold16 => AppTextStyles.interBold16;
+  TextStyle get interBoldItalic16 => AppTextStyles.interBoldItalic16;
+  TextStyle get interMedium18 => AppTextStyles.interMedium18;
+  TextStyle get interSemiBold24 => AppTextStyles.interSemiBold24;
 
   // Add more extensions as needed
 }
--- lib/view/landing_page/landing_page.dart
+++ lib/view/landing_page/landing_page.dart
@@ -1,8 +1,6 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:truebiller/Themes/app_text_theme.dart';
-import 'package:truebiller/constants/constants.dart';
-import 'package:truebiller/utils/size_utils.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -11,48 +9,220 @@ class LandingPage extends StatefulWidget {
   State<LandingPage> createState() => _LandingPageState();
 }
 
-class _LandingPageState extends State<LandingPage> {
+class _LandingPageState extends State<LandingPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 6),
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
         children: [
-          Container(
-            decoration:
-                const BoxDecoration(color: Color.fromARGB(255, 92, 164, 223)),
-            height: SizeUtils.height,
-            width: SizeUtils.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+          Expanded(
+            flex: 1,
+            child: Stack(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    AnimatedTextKit(animatedTexts: [
-                      TypewriterAnimatedText("Welcome !",
-                          textStyle: context.openSansSemiBold24,
-                          speed: Durations.medium4)
-                    ])
-                  ],
+                Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Color(0xFF5CA4DF), Color(0xFF3461C1)],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                ),
+                _buildMovingBubble(
+                    top: 100,
+                    left: 40,
+                    size: 120,
+                    color: Colors.white.withOpacity(0.2)),
+                _buildMovingBubble(
+                    bottom: 150,
+                    left: 150,
+                    size: 80,
+                    color: Colors.white.withOpacity(0.15)),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.receipt_long_rounded,
+                        size: 120,
+                        color: Colors.white.withOpacity(0.9),
+                      ),
+                      const SizedBox(height: 20),
+                      AnimatedTextKit(
+                        animatedTexts: [
+                          TypewriterAnimatedText(
+                            "Welcome to TrueBiller",
+                            textStyle: context.openSansSemiBold24.copyWith(
+                              color: Colors.white,
+                            ),
+                            speed: const Duration(milliseconds: 200),
+                          ),
+                        ],
+                        totalRepeatCount: 1,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
           ),
-          Container(
-            padding: const EdgeInsets.all(paddingLarge),
-            width: double.maxFinite,
-            color: Colors.red,
-            height: SizeUtils.height,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+          Expanded(
+            flex: 1,
+            child: Stack(
               children: [
-                const Text("Enter Organization Name:"),
-                TextFormField()
+                Container(
+                  color: Colors.white,
+                ),
+                _buildMovingBubble(
+                    top: 200,
+                    right: 50,
+                    size: 100,
+                    color: Colors.blue.withOpacity(0.1)),
+                _buildMovingBubble(
+                    bottom: 100,
+                    right: 100,
+                    size: 60,
+                    color: Colors.blue.withOpacity(0.15)),
+                Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(30.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 15,
+                          offset: const Offset(0, 10),
+                        ),
+                      ],
+                    ),
+                    width: 400,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "Set Up Your Organization",
+                          style: context.openSansBold16.copyWith(
+                            color: Colors.black87,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 25),
+                        TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide.none,
+                            ),
+                            hintText: "Enter Organization Name",
+                            hintStyle: const TextStyle(color: Colors.black54),
+                            contentPadding: const EdgeInsets.symmetric(
+                              vertical: 18,
+                              horizontal: 16,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                        MouseRegion(
+                          cursor: SystemMouseCursors.click,
+                          child: AnimatedContainer(
+                            duration: const Duration(milliseconds: 200),
+                            curve: Curves.easeIn,
+                            width: double.infinity,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                colors: [Color(0xFF6FA8DC), Color(0xFF3A79B7)],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  blurRadius: 15,
+                                  offset: const Offset(0, 5),
+                                ),
+                              ],
+                            ),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                backgroundColor: Colors.transparent,
+                                shadowColor: Colors.transparent,
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                "Continue",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
+    );
+  }
+
+  Widget _buildMovingBubble(
+      {double? top,
+      double? bottom,
+      double? left,
+      double? right,
+      required double size,
+      required Color color}) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Positioned(
+          top: top != null ? top + _controller.value * 15 : null,
+          bottom: bottom != null ? bottom + _controller.value * 15 : null,
+          left: left != null ? left + _controller.value * 15 : null,
+          right: right != null ? right + _controller.value * 15 : null,
+          child: Container(
+            width: size,
+            height: size,
+            decoration: BoxDecoration(
+              color: color,
+              shape: BoxShape.circle,
+            ),
+          ),
+        );
+      },
     );
   }
 }
