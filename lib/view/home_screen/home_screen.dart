b14a34b6a2cf1ecc52900cb511f96d5d570dbd23
Aswinazikar <azwinaswin8@gmail.com>
2024-10-23 16:14:04 +0530

    "Updated HomeScreen to use CustomDrawer instead of default Drawer"

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:truebiller/constants/constants.dart';
+import 'package:truebiller/view/home_screen/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +13,10 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(),
+      drawer: CustomDrawer(
+        backgroundColor: Colors.transparent,
+        gradientColor: buttonGradient,
+      ),
       appBar: AppBar(),
     );
   }
